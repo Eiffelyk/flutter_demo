1560fe759ec6bd0a5f69e2159d56c57f37b4b1f1
馋猫 <donghai.kang@dataenlighten.com>
2019-07-27 00:45:20 +0800

    获取textField的内容

---
+++ lib/forms/retrievethetext.dart
@@ -0,0 +1,67 @@
+import 'package:flutter/material.dart';
+
+class RetrieveTheTextDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Retrieve The Text",
+      home: RetrieveTheText(),
+    );
+  }
+}
+
+class RetrieveTheText extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => RetrieveTheTextState();
+}
+
+class RetrieveTheTextState extends State<RetrieveTheText> {
+  TextEditingController textFieldController;
+
+  @override
+  void initState() {
+    textFieldController = TextEditingController();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    textFieldController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Retrieve the text"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: TextField(
+          controller: textFieldController,
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.text_fields),
+        tooltip: "show me TextField value",
+        onPressed: () {
+          return showDialog(
+            context: context,
+            builder: (context) {
+              return AlertDialog(
+                content: Text(textFieldController.text),
+                title: Text("Value"),
+                actions: <Widget>[
+                  RaisedButton(
+                    child: Text("que"),
+                  )
+                ],
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -11,6 +11,8 @@ import 'design/tabbardemo.dart';
 import 'forms/mycustomform.dart';
 import 'forms/mycustomtextformfield.dart';
 import 'forms/textfieldfocus.dart';
+import 'forms/retrievethetext.dart';
+
 //void main() => runApp(MyApp());//默认demo
 //void main() => runApp(AnimatedContainerApp());//Container 里的动画渐变效果
 //void main() => runApp(AnimatedOpacityDemo()); //Widget 的淡入淡出效果
@@ -22,4 +24,5 @@ import 'forms/textfieldfocus.dart';
 //void main() => runApp(TabBarDemo());//使用 tabs
 //void main() => runApp(EFCustom());//响应文本框内容的更改
 //void main() => runApp(EFTextFormField());//构建一个有验证判断的表单
-void main() => runApp(MyTextFieldFocusDome());//焦点和文本框
+//void main() => runApp(MyTextFieldFocusDome());//焦点和文本框
+void main() => runApp(RetrieveTheTextDemo()); //获取文本框的输入值
